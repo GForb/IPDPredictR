116f80ea984ea330164a2f4c59a4f337c9047c71
GForb <gordonjohnforbes@gmail.com>
2023-10-19 17:12:49 +0100

    Fixing bugs in formaters and running check

--- R/formaters.R
+++ R/formaters.R
@@ -3,7 +3,7 @@ results_flextable <- function(results_df) {
   n_rows = length(results_df)
   hlines_at = 3* (1:(n_rows -1)/3)
   results_df |> 
-    compress_columns |> 
+    compress_columns() |> 
     flextable::flextable() |> 
     flextable::merge_v(j = "model") |> 
     flextable::hline(i = hlines_at) |> 
@@ -12,8 +12,8 @@ results_flextable <- function(results_df) {
 }
 
 compress_columns <- function(results_df, digits = 2) {
-  results_df |> dplyr::mutate(ci = form_interval(ci.lb, ci.ub, digits))
-  |> dplyr::mutate(pi = form_interval(pi.lb, pi.ub, digits)) |> 
+  results_df |> dplyr::mutate(ci = form_interval(ci.lb, ci.ub, digits)) |> 
+    dplyr::mutate(pi = form_interval(pi.lb, pi.ub, digits)) |> 
     select(-ci.lb, -ci.ub, -pi.lb, -pi.ub)
   
 }
