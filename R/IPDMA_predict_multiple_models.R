0ff65050879fb514902d9b765840a0557dc48644
GForb <48352959+GForb@users.noreply.github.com>
2024-06-14 16:07:50 +0100

    Removing multiple models function as needs editing with change in syntax and not used

--- R/IPDMA_predict_multiple_models.R
+++ R/IPDMA_predict_multiple_models.R
@@ -1,42 +1,42 @@
-IPDMA_predict_multiple_models <- function(model_function_list, 
-                                          model_labels = NULL, 
-                                          data , 
-                                          InternalExternalCV = TRUE, 
-                                          predict_function = predict, 
-                                          evaluate_performance, 
-                                          test_data = NULL, 
-                                          out_var_name, 
-                                          study_var_name = "studyid") {
-  if(!is.null(model_labels)){
-    if(!label_check(model_function_list, model_labels)) {
-      stop("Model function list must be same length as model_labels")
-    }
-  }
-
-  if(InternalExternalCV){
-    
-  } else {
-    results_list <- lapply(model_function_list, 
-        function(model_function)  ipdma_prediction_pipeline(
-                                    data = data, 
-                                    model_function = model_function,
-                                    InternalExternalCV = FALSE,
-                                    predict_function = predict_function,
-                                    evaluate_performance = evaluate_performance,
-                                    test_data = test_data,
-                                    study_var_name = study_var_name,
-                                    out_var_name = out_var_name)
-        )
-  }
-  results_df <- dplyr::bind_rows(results_list, .id = "model") |> 
-    dplyr::select(model, dplyr::everything())
-  
-  if(!is.null(model_labels)){
-    results_df <-  results_df |> add_names_to_results(model_labels)
-  }
-  
-  return(results_df)
-}
+# IPDMA_predict_multiple_models <- function(model_function_list, 
+#                                           model_labels = NULL, 
+#                                           data , 
+#                                           InternalExternalCV = TRUE, 
+#                                           predict_function = predict, 
+#                                           evaluate_performance, 
+#                                           test_data = NULL, 
+#                                           out_var_name, 
+#                                           study_var_name = "studyid") {
+#   if(!is.null(model_labels)){
+#     if(!label_check(model_function_list, model_labels)) {
+#       stop("Model function list must be same length as model_labels")
+#     }
+#   }
+# 
+#   if(InternalExternalCV){
+#     
+#   } else {
+#     results_list <- lapply(model_function_list, 
+#         function(model_function)  ipdma_prediction_pipeline(
+#                                     data = data, 
+#                                     model_function = model_function,
+#                                     InternalExternalCV = FALSE,
+#                                     predict_function = predict_function,
+#                                     evaluate_performance = evaluate_performance,
+#                                     test_data = test_data,
+#                                     study_var_name = study_var_name,
+#                                     out_var_name = out_var_name)
+#         )
+#   }
+#   results_df <- dplyr::bind_rows(results_list, .id = "model") |> 
+#     dplyr::select(model, dplyr::everything())
+#   
+#   if(!is.null(model_labels)){
+#     results_df <-  results_df |> add_names_to_results(model_labels)
+#   }
+#   
+#   return(results_df)
+# }
 
 label_check <- function(list, labels) {
   if(length(list) != length(labels)){
